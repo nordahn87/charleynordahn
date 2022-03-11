0fb625d445506aa752b59fe53c68f112acf7cbe4
nordahn87 <charleynordahn87@gmail.com>
2022-03-11 20:43:14 +0100

    Added svg to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,11 @@
+import Rhombus from '../svg/rhombus'
+
 const Header = () => {
     return (
         <header className="h-screen">
+            <div className="h-full w-full flex justify-center items-center absolute">
+                <Rhombus />
+            </div>
             <section className="grid grid-cols-2">
                 <div className="bg-blue h-screen"></div>
                 <div className="bg-orange h-screen"></div>
